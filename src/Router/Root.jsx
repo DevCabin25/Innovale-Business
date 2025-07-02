7fe6d2744e2f35a30f4c3014490909325bc48337
Asadulla56 <mdasadullahoque294@gmail.com>
2025-07-02 21:03:38 +0600

    Footer section set up done

--- src/Router/Root.jsx
+++ src/Router/Root.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Navbar from '../Components/Layout/Navbar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Components/Layout/Footer';
 
 const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <main className="min-h-screen">
+                <Outlet></Outlet>
+            </main>
+            <Footer></Footer>
+
         </div>
     );
 };
